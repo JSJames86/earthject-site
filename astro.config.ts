43edf74be053e0b691de21e26bf93ed4f4792be1
Janelle Glanville <janelles.glanville@gmail.com>
2025-09-05 18:41:31 -0400

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     mdx(),
     icon({
       include: {
-        tabler: ['*'],
+        'tabler:*': true,
         'flat-color-icons': [
           'template',
           'gallery',
